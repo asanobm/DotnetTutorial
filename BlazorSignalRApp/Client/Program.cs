e9f12042bc9d154d8ee322486599c9cbf8400a33
Asanobm <kim_bumin@fourglobe.co.jp>
2020-12-08 00:08:02 +0900

    Hosted Blazor WebAssembly Web App with SignalR

--- BlazorSignalRApp/Client/Program.cs
+++ BlazorSignalRApp/Client/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace BlazorSignalRApp
+namespace BlazorSignalRApp.Client
 {
     public class Program
     {
@@ -17,8 +17,7 @@ namespace BlazorSignalRApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(
-                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
---
+++ BlazorSignalRApp/Server/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿//
+//  ChatHub.cs
+//
+//  Author:
+//       Asanobm <kim_bumin@fourglobe.co.jp>
+//
+// 7/12/2020 
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
